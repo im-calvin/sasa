6801ac260dfdaa45d7574517b97121efef90c330
imcalvin <kelvinwong0519@gmail.com>
2025-04-04 20:28:13 -0700

    wip: add screenshot logic to video

--- app/camera/page.tsx
+++ app/camera/page.tsx
@@ -1,11 +1,14 @@
 "use client";
-import { memo, useEffect, useState } from "react";
+import { memo, RefObject, useEffect, useRef, useState } from "react";
 import { Video } from "@/components/Video";
 
-const VideoMemo = memo(Video);
+const VideoMemo = memo(Video); // memoize so that video component doesn't rerender and flash
 
-export default function Camera() {
-  const [time, setTime] = useState(0); // NOTE: this is a little hacky, it should be synced on the camera permissions
+export default function CameraPage() {
+  const [time, setTime] = useState(0);
+
+  // notify the child to take a screenshot
+  function takeScreenshot() {}
 
   useEffect(() => {
     const timerId = setInterval(() => {
@@ -17,13 +20,13 @@ export default function Camera() {
   useEffect(() => {
     if (time === 0) {
       // take a snapshot
-      
+      const image = takeScreenshot();
     }
-  }, [time])
+  }, [time]);
 
   return (
     <div className="flex items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <VideoMemo setTime={setTime} />
+      <VideoMemo setTime={setTime} takeScreenshot={takeScreenshot} />
       {time <= 5 && time > 0 && (
         <div className="absolute inset-0 flex items-center justify-center">
           <div className="text-4xl font-bold text-white">{time}</div>
--- components/Video.tsx
+++ components/Video.tsx
@@ -1,13 +1,19 @@
 "use client";
-import { useRef, useEffect, useState } from "react";
+import { trackFallbackParamAccessed } from "next/dist/server/app-render/dynamic-rendering";
+import { useRef, useEffect, useState, RefObject } from "react";
 import { Dispatch, SetStateAction } from "react";
 
 type VideoPropsT = {
   setTime: Dispatch<SetStateAction<number>>;
+  takeScreenshot: (
+    video: RefObject<HTMLVideoElement>,
+    canvasRef: RefObject<HTMLVideoElement>
+  ) => void;
 };
 
-export function Video({setTime}: VideoPropsT) {
-  const streamRef = useRef<MediaStreamTrack[] | null>(null);
+export function Video({ setTime, takeScreenshot }: VideoPropsT) {
+  const streamRef = useRef<MediaStream | null>(null);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
   const [isStreamReady, setIsStreamReady] = useState(false);
 
   async function requestCameraPermission() {
@@ -16,7 +22,7 @@ export function Video({setTime}: VideoPropsT) {
         video: true,
       });
       console.log("Camera permission granted");
-      streamRef.current = stream.getVideoTracks();
+      streamRef.current = stream;
       setIsStreamReady(true);
       setTime(6);
     } catch (error) {
@@ -26,16 +32,42 @@ export function Video({setTime}: VideoPropsT) {
 
   useEffect(() => {
     requestCameraPermission();
+
+    return () => {
+      if (streamRef.current) {
+        streamRef.current.getTracks().forEach((track) => track.stop());
+      }
+    };
   }, []);
+
+  // whenever the parent triggers takeScreenshot, this component takes a screenshot and then passes it back up
+  function screenshot() {
+    if (!streamRef.current || !videoRef.current) {
+      return;
+    }
+
+    const canvas = document.createElement("canvas");
+    const video = videoRef.current;
+    const context = canvas.getContext("2d");
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    context?.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    const imageDataUrl = canvas.toDataURL("image/png");
+
+    console.log(imageDataUrl);
+  }
 
   return isStreamReady ? (
     <video
       autoPlay
-      ref={(video) => {
-        if (video && streamRef.current) {
-          video.srcObject = new MediaStream(streamRef.current);
+      ref={videoRef}
+      onLoadedMetadata={() => {
+        if (streamRef.current) {
+          videoRef.current!.srcObject = streamRef.current; // Set the stream to the video element
         }
       }}
+      className="w-full"
     />
   ) : (
     <p>Please allow camera access</p>
