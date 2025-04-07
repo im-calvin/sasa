ef3d1dda7b8b5c5f1d1cf53ee76744f487955f8d
imcalvin <kelvinwong0519@gmail.com>
2025-04-07 02:21:14 -0700

    add font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Instrument_Serif } from "next/font/google";
 import "./globals.css";
 import { ScreenshotsProvider } from "@/lib/ScreenshotsContext";
 import { FrameProvider } from "@/lib/FrameContext";
@@ -11,6 +11,12 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const instrumentSerif = Instrument_Serif({
+  variable: "--font-instrument-serif",
+  weight: "400",
   subsets: ["latin"],
 });
 
