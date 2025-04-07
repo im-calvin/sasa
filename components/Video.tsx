6f2e6bdbd2029ec15af31b5aabe019d578bec049
imcalvin <kelvinwong0519@gmail.com>
2025-04-06 23:41:28 -0700

    fix eslint

--- components/Video.tsx
+++ components/Video.tsx
@@ -8,9 +8,7 @@ import {
   useImperativeHandle,
 } from "react";
 import { Dispatch, SetStateAction } from "react";
-// because we are just doing vanilla css stuff :D
-// @ts-expect-error
-import styles from "@/styles/Video.module.scss";
+import "@/styles/Video.module.scss";
 
 type VideoPropsT = {
   setTime?: Dispatch<SetStateAction<number>>;
