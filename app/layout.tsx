3012ea84cc1ab5c1af0ad0b703d82c12a12e1408
imcalvin <kelvinwong0519@gmail.com>
2025-04-07 02:22:07 -0700

    fix instrumentserif

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        className={`${geistSans.variable} ${geistMono.variable} ${instrumentSerif.variable} antialiased`}>
         <ScreenshotsProvider>
           <FrameProvider>{children}</FrameProvider>
         </ScreenshotsProvider>
