5638dcff7a472e5adb941a7e7e647fa1d9cac90a
Kelvin Wong <61614687+im-calvin@users.noreply.github.com>
2025-04-12 16:25:11 -0700

    Camera and frame :> (#1)
    * add flash animation :D
    
    * this shit sucks
    
    * this shit sucks p2
    
    * camera :D
    
    * frame page :<
    
    * aspect ratio fixed :D

--- app/camera/page.tsx
+++ app/camera/page.tsx
@@ -10,18 +10,22 @@ import {
 import { useRouter } from "next/navigation";
 import { useScreenshots } from "@/lib/ScreenshotsContext";
 import PrimaryButton from "@/components/PrimaryButton";
-import Footer from "@/components/Footer";
 import { Loading } from "@/components/Loading";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 
 const VideoMemo = memo(Video); // memoize so that video component doesn't rerender and flash
 
 export default function CameraPage() {
   const router = useRouter();
   const [time, setTime] = useState(-1);
-  const videoRef = useRef<{ getScreenshot: () => string } | null>(null);
+  const videoRef = useRef<{
+    getScreenshot: (aspectRatioElement: HTMLElement) => string;
+  } | null>(null);
   const { addScreenshot, screenshots } = useScreenshots();
   const [isTakingPhotos, setIsTakingPhotos] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [flash, setFlash] = useState<boolean>(false);
+  const aspectRatioRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     const timerId = setInterval(() => {
@@ -31,18 +35,14 @@ export default function CameraPage() {
   }, []);
 
   useEffect(() => {
-    if (time === 0 && videoRef.current) {
+    if (time === 0 && videoRef.current && aspectRatioRef.current) {
+      setFlash(true);
+      setTimeout(() => {
+        setFlash(false);
+      }, FLASH_DURATION);
+
       // take a snapshot
-      const image = videoRef.current.getScreenshot();
-      // TODO do fancy css animation to appear like a screenshot was taken <3
-      // Add a flash animation when screenshot is taken
-      const flashElement = document.querySelector(".flash");
-      if (flashElement) {
-        flashElement.classList.add("flashing");
-        setTimeout(() => {
-          flashElement.classList.remove("flashing");
-        }, FLASH_DURATION); // Short duration for the flash effect
-      }
+      const image = videoRef.current.getScreenshot(aspectRatioRef.current);
 
       addScreenshot(image);
       setTime(RESET_TIME); // reset the timer
@@ -61,52 +61,70 @@ export default function CameraPage() {
   }, [screenshots, router]);
 
   return (
-    <div className="grid max-h-dvh min-h-svh grid-rows-[20px_10px_10px_1fr_20px_10px] items-center justify-items-center gap-y-8 py-8">
-      <header className="row-start-1">
-        <h5>{"SAMANTHA'S PHOTO CORNER"}</h5>
-      </header>
+    <div className="relative grid max-h-lvh min-h-lvh grid-rows-[auto_auto_auto] items-center justify-items-center overflow-hidden">
+      {flash && (
+        <div className="animate-fade-out bg-background pointer-events-none absolute inset-0 z-99 min-h-lvh opacity-100" />
+      )}
+      {!isLoading && (
+        <>
+          {/* top banner */}
+          <div className="row-start-1 h-full w-full bg-black" />
+          {/* bottom banner */}
+          <div className="row-start-3 h-full w-full bg-black" />
+        </>
+      )}
+      {/* actual content */}
       {isLoading ? (
-        <div className="row-start-4">
-          <Loading text={"Lookin' goooood ;)"} />
+        <div className="row-start-2">
+          <Loading text={"Great choices!"} />
         </div>
       ) : (
         <>
-          {!isTakingPhotos && <h3 className="row-start-2">STEP 3</h3>}
-          <h2 className="row-start-3">
-            {isTakingPhotos ? "Smile!" : "Start Snapping"}
-          </h2>
-          <main className="row-start-4 w-full">
-            <div className="relative py-6">
-              <VideoMemo ref={videoRef} />
-              {time <= 5 && time > 0 && (
-                <div className="absolute inset-0 flex items-center justify-center">
-                  <p className="text-[3rem] text-white">{time}</p>
-                </div>
-              )}
-            </div>
-            {/* <p className="h-fit-content flex w-full justify-center text-center">
-              {isTakingPhotos
-                ? `${screenshots.length}/${MAX_PHOTOS}`
-                : `You'll get ${MAX_PHOTOS} photos with ${RESET_TIME} seconds to pose each time`}
-            </p> */}
-          </main>
-          <div className="row-start-5 flex w-full flex-row justify-center">
-            {!isTakingPhotos && (
-              <PrimaryButton
-                disable={time === -1}
-                onClick={() => {
-                  setTime(RESET_TIME);
-                }}
-              >
-                <h4>{"I'm ready"}</h4>
-              </PrimaryButton>
+          <div className="inset-0 -z-1 col-start-1 -col-end-1 row-start-2 h-full">
+            <VideoMemo
+              ref={videoRef}
+              className={`${isTakingPhotos ? "" : "scale-[1.1] transform blur-[2px] brightness-50"}`}
+            />
+            {time <= 5 && time > 0 && (
+              <div className="absolute inset-0 z-0 flex w-screen items-center justify-center">
+                <p className="text-[3rem] text-white">{time}</p>
+              </div>
             )}
           </div>
+
+          <main className="col-start-1 -col-end-1 row-start-2 w-full text-white">
+            <AspectRatio
+              ref={aspectRatioRef}
+              ratio={4 / 5}
+              className="flex flex-col items-center justify-center gap-32 pt-10 pb-4"
+            >
+              <div
+                className={`flex flex-col items-center ${isTakingPhotos ? "invisible" : ""}`}
+              >
+                <h3>Step 3</h3>
+                <h2>Start Snapping</h2>
+              </div>
+              <div
+                className={`flex flex-col items-center justify-center gap-8 ${isTakingPhotos ? "invisible" : ""}`}
+              >
+                <p className="w-3/4 text-center">
+                  {`You'll get ${MAX_PHOTOS} photos with ${RESET_TIME} seconds to pose each time`}
+                </p>
+                <PrimaryButton
+                  disable={time === -1}
+                  onClick={() => {
+                    setTime(RESET_TIME);
+                  }}
+                  className="text-black"
+                >
+                  <h4>{"I'm ready"}</h4>
+                </PrimaryButton>
+              </div>
+              <div className="bottom-4 text-white">{`${screenshots.length}/${MAX_PHOTOS}`}</div>
+            </AspectRatio>
+          </main>
         </>
       )}
-      <footer className="row-start-6">
-        <Footer />
-      </footer>
     </div>
   );
 }
