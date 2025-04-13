71b4b74b4bc2f6ec73fc4d7b9216c8c96c04776c
im-calvin <kelvinwong0519@gmail.com>
2025-04-13 15:53:19 -0700

    fix home page styling

--- app/page.tsx
+++ app/page.tsx
@@ -5,35 +5,30 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid min-h-svh grid-rows-[20px_10px_10px_1fr_20px_10px] items-center justify-items-center gap-8 p-8 sm:p-20">
-      <header className="row-start-1">
-        <h5>IDEA GRAD SHOW 2025</h5>
+    <div className="grid max-h-svh min-h-svh grid-rows-[10svh_auto_13svh] items-center justify-items-center">
+      <header className="row-start-1 flex flex-col items-center justify-center">
+        <h5 className="pt-2">{"IDEA GRAD SHOW 2025"}</h5>
       </header>
-      <main className="row-start-4 flex flex-col items-center gap-[32px]">
-        <h1>Photo Corner</h1>
-        <Image
-          src="/home.png"
-          className="black"
-          alt="Digital Frames"
-          width={1080}
-          height={300}
-          priority
-        />
-        <p className="w-4/5 text-center">
-          Thanks for coming! Take some photos and enjoy these digital frames I
-          designed~
-        </p>
+      <main className="row-start-2 flex h-full flex-col items-center justify-center">
+        <div className="relative aspect-[428/633] h-full">
+          <Image
+            src="/home.png"
+            className="aspect-frame relative mx-auto h-full overflow-hidden object-cover"
+            alt="Digital Frames"
+            fill
+            priority
+            quality={100}
+          />
+        </div>
       </main>
-      <div className="row-start-5 flex flex-row justify-center gap-4">
+      <div className="row-start-3 flex w-full flex-col items-center justify-start gap-2">
         <PrimaryButton>
           <Link href="/frame">
             <h4>Start</h4>
           </Link>
         </PrimaryButton>
-      </div>
-      <footer className="row-start-6">
         <Footer />
-      </footer>
+      </div>
     </div>
   );
 }
